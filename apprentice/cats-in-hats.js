71c04cf4bb5f28047955c86cc52db3def7099290
Alan-Miller <alan.ashman@gmail.com>
2018-01-04 18:24:43 -0700

    edits

--- apprentice/cats-in-hats.js
+++ apprentice/cats-in-hats.js
@@ -2,15 +2,15 @@
   CATS IN HATS
   https://www.codewars.com/kata/cats-in-hats/train/javascript
 
-  The Cat In The Hat has cat A under his hat, cat A has cat B under his hat and so on until Z
+  The Cat in the Hat has cat A under his hat, cat A has cat B under his hat and so on until Z
 
-  The Cat In The Hat is 2,000,000 cat units tall.
+  The Cat in the Hat is 2,000,000 cat units tall.
 
   Each cat is 2.5 times bigger than the cat underneath their hat.
 
   Find the total height of the cats if they are standing on top of one another.
 
-  Counting starts from the Cat In The Hat
+  Counting starts from the Cat in the Hat
 
   n = the number of cats
 
