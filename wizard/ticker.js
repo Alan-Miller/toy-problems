547981274f4f291cb360ce55efde3be2874014b3
Alan-Miller <alan.ashman@gmail.com>
2018-04-11 23:22:12 -0600

    ticker problem

---
+++ wizard/ticker.js
@@ -0,0 +1,12 @@
+/*
+  Write a function that accepts an array of 24 numbers. 
+  These numbers are tomorrow's prices for a given stock, one for each hour.
+  Your function should determine the best time to buy and sell.
+  HINT: You can't sell before you buy.
+  Return an array with the buy time and sell time.
+*/
+
+// let prices = [34, 39, 35, 37, 2, 200, 1000, 23, 77, 500, 1, 39, 44, 21]
+// let prices = [1051, 1070, 2000, 77, 50, 949, 1000, 23, 1, 10]
+const t = [10, 9, 8, 7, 6, 5, 5, 5, 7, 5, 8, 4, 4, 3, 2, 1, 1, 3, 8, 7, 7, 1, 7, 5]
+// const t = [5, 5, 5, 7, 5, 8, 4, 4, 3, 2, 1, 1, 3, 8, 7, 7, 1, 7, 5, 7, 7, 10, 3, 1]
